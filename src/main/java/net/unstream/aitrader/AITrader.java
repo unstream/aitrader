9d44de09316db869e03904115a8f2fe21cff44c5
Frank Erbsen <erbsen@th-wildau.de>
2018-06-26 11:39:14 +0200

    master: solved merge conflict - csv generation was lost

--- src/main/java/net/unstream/aitrader/AITrader.java
+++ src/main/java/net/unstream/aitrader/AITrader.java
@@ -1,11 +1,8 @@
 package net.unstream.aitrader;
 
-import net.unstream.aitrader.strategies.CrashDetectionStrategy;
-import net.unstream.aitrader.strategies.KI1Strategy;
-import net.unstream.aitrader.strategies.KI2Strategy;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVRecord;
-
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -15,69 +12,75 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 
+import net.unstream.aitrader.strategies.After3IncreasesStrategy;
+import net.unstream.aitrader.strategies.AfterOneIncreasesStrategy;
+import net.unstream.aitrader.strategies.BuyAndHoldStrategy;
+import net.unstream.aitrader.strategies.InverseStrategy;
+import net.unstream.aitrader.strategies.CrashDetectionStrategy;;
 
 public class AITrader {
 	private void runDax() throws IOException {
 		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
 		InputStream is = classloader.getResourceAsStream("GDAXI.csv");
-
-		Account myAccount = Account.builder()
-				.cash(1000)
-				.strategy(new KI2Strategy())
-				.build();
 		
+		
+		File outFile = new File("src/main/resources/result.csv");
+		outFile.createNewFile();
+		BufferedWriter out = new BufferedWriter(new FileWriter(outFile));
+
+		CSVPrinter csvPrinter = new CSVPrinter(out, CSVFormat.DEFAULT.withHeader("time", "balance", "dax"));
+
+		Account myAccount = Account.builder().cash(1000).strategy(new BuyAndHoldStrategy()).build();
+
 		Reader in = new InputStreamReader(is);
+
 		Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader().parse(in);
 		List<DaxData> dax = new ArrayList<DaxData>();
-		int i = 0;
+		int time = 1;
+
 		for (CSVRecord record : records) {
 
 			tradeForTheDay(myAccount, dax);
 
 			String dateText = record.get("Date");
-		    String open = record.get("Open");
-		    SimpleDateFormat parser=new SimpleDateFormat("yyyy-MM-dd");
-		    try {
-				dax.add(DaxData.builder()
-						.date(parser.parse(dateText))
-						.open(Double.parseDouble(open))
-						.build()
-				);
+			String open = record.get("Open");
+			SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd");
+			try {
+				dax.add(DaxData.builder().date(parser.parse(dateText)).open(Double.parseDouble(open)).build());
 			} catch (NumberFormatException | ParseException e) {
 				continue;
 			}
 
 			updateAccount(myAccount, dax);
+			System.out.println("Time: "+time);
 			System.out.println("Dax: " + dax.get(dax.size() - 1).getOpen());
+			csvPrinter.printRecord(time, String.valueOf(myAccount.balance()),dax.get(dax.size() - 1).getOpen());
 			System.out.println("Balance: " + myAccount.balance());
-
+			time++;
 		}
-		System.out.println("Dax.size(): " + dax.size());
+		
+		csvPrinter.flush(); 
 	}
-	
+
 	public void updateAccount(Account account, List<DaxData> dax) {
 		if (dax.size() > 1) {
 			account.setShares(
-					account.getShares() * dax.get(dax.size() - 1).getOpen() 
-					/ dax.get(dax.size() - 2).getOpen());
+					account.getShares() * dax.get(dax.size() - 1).getOpen() / dax.get(dax.size() - 2).getOpen());
 		}
 	}
 
 	public void tradeForTheDay(Account myAccount, List<DaxData> dax) {
-		if (dax.size() < 4000 ) { //4000
+		if (myAccount.getStrategy().invest(dax)) {
 			myAccount.invest();
 		} else {
-			if (myAccount.getStrategy().invest(dax)) {
-				myAccount.invest();
-			} else {
-				myAccount.cashout();
-			}
+			myAccount.cashout();
 		}
 	}
 
-	
 	public static void main(String[] args) throws IOException {
 		AITrader ai = new AITrader();
 		ai.runDax();
